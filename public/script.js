d90dad6afb430ed3082dd22e667847bbfddb9918
Melissa Seago <melissa.seago@gmail.com>
2016-04-22 09:46:05 -0500

    updated html and css

--- public/script.js
+++ public/script.js
@@ -1,1 +1,26 @@
-console.log("hello from the js file!")
+'use strict';
+if (this.ToDoApp === undefined) this.ToDoApp = {};
+
+(function(context) {
+
+function createListItems(list) {
+  for (var x = 0; x < list.length; x++) {
+    var li = document.createElement('li');
+    li.textContent = item;
+    toDoList.appendChild(li);
+  }
+  return li;
+}
+
+
+
+
+
+  function start() {
+
+    var doneButton = document.querySelector('#done-button');
+  }
+
+  window.ToDoApp.start = start;
+
+})(window.ToDoApp);
